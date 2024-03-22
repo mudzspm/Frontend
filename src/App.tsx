0443e377b686015c3b8b396fe8e5681164c83cd1
asifmehmood101 <amehmood999@gmail.com>
2024-03-22 14:44:25 +0500

    Ui-addendum

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-// import { Home } from './pages/home/home';
-// import { ContactUs } from './pages/contactUs/contactUs';
+import { Home } from './pages/home/home';
+
 
 import './App.css';
-import Achievement from './pages/achievement/achievement';
-import CountryImplementation from './pages/countryImplementation/countryImplementation';
-import Empty from './pages/emptyCart/empty';
+
 function App() {
   return (
     <>
       <div>
-        {/* <Home /> */}
-        {/* <ContactUs /> */}
-        {/* <CountryImplementation /> */}
-        {/* <Achievement /> */}
+        <Home />
+      
 
-        <Empty />
-
+   
       </div>
     </>
   );
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,21 +1,8 @@
-import Trust from '@/pages/home/sections/trust';
-import Hero from '@/pages/home/sections/hero';
-import Course from '@/pages/home/sections/course';
-import Achievements from '@/pages/home/sections/achievements';
-import PageWrapper from '@/components/ui/PageWrapper';
-import QurbanService from '@/pages/home/sections/qurbanServicebanner';
-import Services from '@/pages/home/sections/Services';
+
 
 export const Home = () => {
   return (
-    <PageWrapper>
-      <Hero />
-      <Course />
-      <Trust />
-      <Achievements />
-      <QurbanService />
-      <Services />
-    </PageWrapper>
-  );
-};
-export default Home;
+    <div>Home</div>
+  )
+}
+export default Home
